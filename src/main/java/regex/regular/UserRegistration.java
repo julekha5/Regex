019f259233eb120ac2ca1448bf28b77ef8afd8e7
Julekha5 <mulani.julekha93@gmail.com>
2021-12-12 15:51:36 +0530

    Added: Mobile number regex pattern

--- src/main/java/regex/regular/UserRegistration.java
+++ src/main/java/regex/regular/UserRegistration.java
@@ -4,14 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Check user email with regex
+ * Check user mobile number with regex
  */
 public class UserRegistration {
-	// method for email name pattern
-	public boolean isValidEmail(String email) {
-		String regex = "^[a-z.+_-]+[.a-z0-9]*+@[a-z0-9]+[.a-z]+[.a-z0-9]*$";
+	// method for mobile number pattern
+	public boolean isMobileNumber(String phone) {
+		String regex = "^[0-9]{2}+[ ][0-9]{10}$";
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(email);
+		Matcher matcher = pattern.matcher(phone);
 		if (matcher.matches()) {
 			return true;
 		}
@@ -21,14 +21,7 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		UserRegistration user = new UserRegistration();
 
-		System.out.println(user.isValidEmail("abc@yahoo.com"));
-		System.out.println(user.isValidEmail("abc-100@yahoo.com"));
-		System.out.println(user.isValidEmail("abc.100@yahoo.com"));
-		System.out.println(user.isValidEmail("abc111@abc.com"));
-		System.out.println(user.isValidEmail("abc-100@abc.net"));
-		System.out.println(user.isValidEmail("abc.100@abc.com.au"));
-		System.out.println(user.isValidEmail("abc@1.com"));
-		System.out.println(user.isValidEmail("abc@yahoo.com.com"));
-		System.out.println(user.isValidEmail("abc+100@yahoo.com"));
+		System.out.println(user.isMobileNumber("91 9999999999"));
+		System.out.println(user.isMobileNumber("919999999999"));
 	}
 }
