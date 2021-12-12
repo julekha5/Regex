ecca54c41169fd31a4cf487e4184d4ab2a2cc72d
Julekha5 <mulani.julekha93@gmail.com>
2021-12-12 15:23:11 +0530

    Added: Firsr name regex pattern

--- src/main/java/regex/regular/UserRegistration.java
+++ src/main/java/regex/regular/UserRegistration.java
@@ -1,4 +1,5 @@
 package regex.regular;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -6,20 +7,20 @@ import java.util.regex.Pattern;
  * Check user first name with regex
  */
 public class UserRegistration {
-    //method for check firstname pattern
+	// method for check firstname pattern
 	public boolean isFirstName(String fname) {
-        String regex = "^[A-Z][a-z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(fname);
-        if (matcher.matches()) {
-            return true;
-        }
-        return false;
-    }
+		String regex = "^[A-Z][a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(fname);
+		if (matcher.matches()) {
+			return true;
+		}
+		return false;
+	}
 
-    public static void main(String[] args) {
-    	UserRegistration first = new UserRegistration();
-        System.out.println(first.isFirstName("Julekha"));
-        System.out.println(first.isFirstName("JulekhaA"));
-    }
+	public static void main(String[] args) {
+		UserRegistration first = new UserRegistration();
+		System.out.println(first.isFirstName("Julekha"));
+		System.out.println(first.isFirstName("JulekhaA"));
+	}
 }
