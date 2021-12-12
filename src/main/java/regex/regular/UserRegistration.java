0fc76053afb07c187a8b98ea2e39d8c1904dcef9
Julekha5 <mulani.julekha93@gmail.com>
2021-12-12 15:45:48 +0530

    Added: Valid email regex pattern

--- src/main/java/regex/regular/UserRegistration.java
+++ src/main/java/regex/regular/UserRegistration.java
@@ -4,14 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Check user last name with regex
+ * Check user email with regex
  */
 public class UserRegistration {
-	// method for check last name pattern
-	public boolean isLastName(String fname) {
-		String regex = "^[A-Z][a-z]{2,}$";
+	// method for email name pattern
+	public boolean isValidEmail(String email) {
+		String regex = "^[a-z.+_-]+[.a-z0-9]*+@[a-z0-9]+[.a-z]+[.a-z0-9]*$";
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(fname);
+		Matcher matcher = pattern.matcher(email);
 		if (matcher.matches()) {
 			return true;
 		}
@@ -20,7 +20,15 @@ public class UserRegistration {
 
 	public static void main(String[] args) {
 		UserRegistration user = new UserRegistration();
-		System.out.println(user.isLastName("Mulani"));
-		System.out.println(user.isLastName("MulaNi"));
+
+		System.out.println(user.isValidEmail("abc@yahoo.com"));
+		System.out.println(user.isValidEmail("abc-100@yahoo.com"));
+		System.out.println(user.isValidEmail("abc.100@yahoo.com"));
+		System.out.println(user.isValidEmail("abc111@abc.com"));
+		System.out.println(user.isValidEmail("abc-100@abc.net"));
+		System.out.println(user.isValidEmail("abc.100@abc.com.au"));
+		System.out.println(user.isValidEmail("abc@1.com"));
+		System.out.println(user.isValidEmail("abc@yahoo.com.com"));
+		System.out.println(user.isValidEmail("abc+100@yahoo.com"));
 	}
 }
