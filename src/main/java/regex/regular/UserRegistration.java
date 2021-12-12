88d31b6ee66feb4357b46aa87ebaf6a4c8a910a5
Julekha5 <mulani.julekha93@gmail.com>
2021-12-12 16:27:28 +0530

    Added:Password Rule4 regex pattern

--- src/main/java/regex/regular/UserRegistration.java
+++ src/main/java/regex/regular/UserRegistration.java
@@ -4,13 +4,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Check user password rule3 with regex
- * min 8 character and at least 1 numeric 
+ * Check user password rule4 with regex
+ * min 8 character
+ * one upper case
+ * one numeric
+ *  at least 1 special character 
  */
 public class UserRegistration {
 	// method for password rule2 pattern
 	public boolean isValidPassword(String pass) {
-        String regex = "^(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9+-_!@#$%^&*(){}'.,]{8,}$";
+        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@%^&*(){}])[a-zA-Z0-9+-_!@#$%^&*(){}'.,]{8,}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(pass);
 		if (matcher.matches()) {
@@ -22,7 +25,7 @@ public class UserRegistration {
 	public static void main(String[] args) {
 		UserRegistration user = new UserRegistration();
 
-		System.out.println(user.isValidPassword("ghjlPp45708"));
+		System.out.println(user.isValidPassword("fdjowiei8776#A"));
 		System.out.println(user.isValidPassword("abvlkkm.-"));
 	}
 }
