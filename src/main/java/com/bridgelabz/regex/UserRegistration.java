22c07dd1ffc83bade14b973ddf4a12c551fada15
Julekha5 <mulani.julekha93@gmail.com>
2021-12-15 22:51:11 +0530

    Added:Parameterized Email Test

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -1,63 +1,25 @@
 package com.bridgelabz.regex;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- * Check user first name with regex
+ *  sample email test
  */
 public class UserRegistration {
-	// method for check firstName pattern
-	public boolean isFirstName(String fname) {
-		String regex = "^[A-Z][a-z]{2,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(fname);
-		if (matcher.matches()) {
-			return true;
-		}
-		return false;
-	}
-	// method for check lastName pattern
-	public boolean isLastName(String fname) {
-		String regex = "^[A-Z][a-z]{2,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(fname);
-		if (matcher.matches()) {
-			return true;
-		}
-		return false;
-	}
 	// method for check validEmail pattern
-	 boolean isValidEmail(String email) {
-			String regex = "^[a-z.+_-]+[.a-z0-9]*+@[a-z0-9]+[.a-z]+[.a-z0-9]*$";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(email);
-			if (matcher.matches()) {
-				return true;
-			}
-			return false;
+	public boolean isValidEmail(String email) {
+		String regex = "^[a-z.+_-]+[.a-z0-9]*+@[a-z0-9]+[.a-z]+[.a-z0-9]*$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		if (matcher.matches()) {
+			return true;
 		}
-	 
-	 // method for check mobileNumber pattern
-	 boolean isMobileNumber(String phone) {
-			String regex = "^[0-9]{2}+[ ][0-9]{10}$";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(phone);
-			if (matcher.matches()) {
-				return true;
-			}
-			return false;
-		}
-	
-	 // method for check password pattern
-	 boolean isValidPassword(String pass) {
-	        String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@%^&*(){}])[a-zA-Z0-9+-_!@#$%^&*(){}'.,]{8,}$";
-			Pattern pattern = Pattern.compile(regex);
-			Matcher matcher = pattern.matcher(pass);
-			if (matcher.matches()) {
-				return true;
-			}
-			return false;
-		}
+		return false;
+	}
 
-
+	public static void main(String[] args) {
+		UserRegistration email = new UserRegistration();
+		System.out.println(email.isValidEmail("abc@yahoo.com"));
+	}
 }
--- src/test/java/com/bridgelabz/regex/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/regex/UserRegistrationTest.java
@@ -1,71 +1,42 @@
 package com.bridgelabz.regex;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class UserRegistrationTest{
-     
-	boolean result;
-	UserRegistration userTest = new UserRegistration();
-	
-	@Test
-	public void firstName_IsValid_PassTest() {
-		result = userTest.isFirstName("Julekha");
-		Assert.assertEquals(true, result);
-	}
-	
-	@Test
-	public void firstName_IsValid_FailTest() {
-		result = userTest.isFirstName("julekha");
-		Assert.assertEquals(true,result);
-	}
-	
-	@Test
-	public void lastName_IsValid_PassTest() {
-		result = userTest.isLastName("Mulani");
-		Assert.assertEquals(true,result);
-	}
-	
-	@Test
-	public void lastName_IsValid_FailTest() {
-		result = userTest.isLastName("MulaNi");
-		Assert.assertEquals(true,result);
-	}
-	
-	@Test
-	public void Email_IsValid_PassTest() {
-		result = userTest.isValidEmail("abc.100@yahoo.com");
-		Assert.assertEquals(true,result);
-	}
-	
-	@Test
-	public void Email_IsValid_FailTest() {
-		result = userTest.isValidEmail("abc1236");
-		Assert.assertEquals(true,result);
-	}
-	
-	@Test
-	public void mobileNumber_IsValid_PassTest() {
-		result = userTest.isMobileNumber("91 9898989978");
-		Assert.assertEquals(true,result);
-	}
-	
-	@Test
-	public void mobileNumber_IsValid_FailTest() {
-		result = userTest.isMobileNumber("991234567890");
-		Assert.assertEquals(true,result);
-	}
-	
-	@Test
-	public void password_IsValid_PassTest() {
-		result = userTest.isValidPassword("fdjowiei8776#A");
-		Assert.assertEquals(true,result);
-	}
-	@Test
-	public void password_IsValid_FailTest() {
-		result = userTest.isValidPassword("abvlkkm.-");
-		Assert.assertEquals(true,result);
-	}
-	
-	
-}	
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class UserRegistrationTest {
+    private UserRegistration email;
+    private final boolean expected;
+    private String email2;
+
+    @Before
+    public void initializeClass(){
+    	email = new UserRegistration();
+    }
+
+    public UserRegistrationTest(UserRegistration email, boolean expected) {
+        this.email = email;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> regexList(){
+        return Arrays.asList(new Object[][] {
+                {"abc@yahoo.com",true} , {"abc-100@yahoo.com",true} ,
+                {"abc.100@yahoo.com",true} , {"abc111@abc.com",true},
+                {"abc-100@abc.net",true} , {"abc.100@abc.com.au",true},
+                {"abc@1.com",true} , {"abc@yahoo.com.com",true},
+                {"abc+100@yahoo.com",true} , {"abc@.com",false}
+        });
+    }
+    @Test
+	public void testUserRegistration(){
+        Assert.assertEquals(expected,email.isValidEmail(email2));
+    }
+}
