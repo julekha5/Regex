4cf83c2fd01a74a399d3d3103580841c5c0eac65
Julekha5 <mulani.julekha93@gmail.com>
2021-12-18 17:19:25 +0530

    Refactor : Firstname Validate Using Lambda Expression

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -1,67 +1,35 @@
 package com.bridgelabz.regex;
 
+import java.util.Scanner;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /*
- *  Custom Exception Test for firstname, lastname, username, password
+ * Use Of Functional Interface and Lambda Expression 
  */
+@FunctionalInterface
+interface IFirstName {
+//To check valid first name method	
+	public boolean isFirstName(String fname);
+}
+
 public class UserRegistration {
 
-	// method for check firstName pattern
-	public boolean isFirstName(String fname) throws CustomException.InvalidFirstName {
+	public boolean isFirstName(String fname) {
 		String regex = "^[A-Z][a-z]{2,}$";
 		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(fname);
-		if (matcher.matches()) {
-			return true;
-		}
-		throw new CustomException.InvalidFirstName("FirstName should be start with cap and length is minimum 3");
+		Predicate<String> predicateObj = pattern.asPredicate();
+		return predicateObj.test(fname);
+		/*
+		 * other logic Predicate<Pattern> predicateObj = matcher ->
+		 * matcher.matcher(fname).matches();
+		 */
 	}
 
-	// method for check lastName pattern
-	public boolean isLastName(String lname) throws CustomException.InvalidLastName {
-		String regex = "^[A-Z][a-z]{2,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(lname);
-		if (matcher.matches()) {
-			return true;
-		}
-		throw new CustomException.InvalidFirstName("LastName should be start with cap and length is minimum 3");
+	public static void main(String[] args) {
+		UserRegistration user = new UserRegistration();
+		System.out.println(user.isFirstName("Julekha")); // true
+		System.out.println(user.isFirstName("julekha")); // false
 	}
-
-	// method for check validEmail pattern
-	boolean isValidEmail(String email) throws CustomException.InvalidEmail {
-		String regex = "^[a-z.+_-]+[.a-z0-9]*+@[a-z0-9]+[.a-z]+[.a-z0-9]*$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(email);
-		if (matcher.matches()) {
-			return true;
-		}
-		throw new CustomException.InvalidEmail("Email should have 3 mandatory parts and 2 optional");
-	}
-
-	// method for check mobileNumber pattern
-	boolean isValidMobileNumber(String phone) throws CustomException.InvalidMobile {
-		String regex = "^[0-9]{2}+[ ][0-9]{10}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(phone);
-		if (matcher.matches()) {
-			return true;
-		}
-		throw new CustomException.InvalidMobile("Country code follwed by space and 10 digit number");
-	}
-
-	// method for check password pattern
-	boolean isValidPassword(String pass) throws CustomException.InvalidPassword {
-		String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!#@%^&*(){}])[a-zA-Z0-9+-_!@#$%^&*(){}'.,]{8,}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(pass);
-		if (matcher.matches()) {
-			return true;
-		}
-		throw new CustomException.InvalidPassword(
-				"Minimum 8 characters, atleast 1 uppercase,1 numeric , exact 1 special character");
-	}
-
 }
